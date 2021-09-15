7896ba80926eb6f6c9c295864c31d3a449d3fffb
tuantruongdev <tuantruong.a8k29@gmail.com>
2021-09-15 09:39:38 +0700

    like func
    added like

--- src/Navigators/Main.tsx
+++ src/Navigators/Main.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { LoginScrren } from '@/Containers'
-import {  Image,Input} from "react-native-elements";
+import {  Image,Input,Button} from "react-native-elements";
 import Icon from 'react-native-vector-icons/Ionicons';
-import { View ,Text,Button,FlatList, Alert} from 'react-native';
+import { View ,Text,FlatList, Alert,TouchableOpacity} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
 import firestore from '@react-native-firebase/firestore';
@@ -16,25 +16,80 @@ const Tab = createBottomTabNavigator()
 var accessToken:any;
 
 
+
 const HomeScreen=({navigation}:any)=>{
 
-  const [iconName,setIconName]= useState("heart-outline")
-  const [count,setCount]=useState(1);
+  const [randomshit,setrd]=useState(1);
+  const [listeningDB,setListeningDB] =useState(false);
+  useEffect(() => {
+    if(listeningDB==false){
+      dosomeShit();
+      setListeningDB(true);
+    }
+  });//listening db once load
+
+
+
+
+ 
   const [data,setData]=useState([] as any);
   const [stt,setStt]= useState("");
-  function onResult(QuerySnapshot:any) {
-    console.log('Got Users collection updates.');
+
+  async function onResult(QuerySnapshot:any) {
+    const user = auth().currentUser?.uid  as any;
+  //  console.log('Got Users collection updates.');
     var temp=[]as any;
-    QuerySnapshot.forEach((documentSnapshot: { id: any; data: () => any; })=> {
+    var liker_list={} as any;
+    QuerySnapshot.forEach(async (documentSnapshot: { id: any; data: () => any; })=> {
+      var liked=0;
+      var like_count=0;
     //  console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
+   try{
+   //  console.log(documentSnapshot.id);
+   const post_likers = await firestore().collection('post-likers').doc(documentSnapshot.id).collection("0").doc(user).get();
+   const post_likers_count = await firestore().collection('post-likers').doc(documentSnapshot.id).collection("0").get();
+  
+   console.log(liker_list);
+   like_count=post_likers_count.size;
+
    
+   if(post_likers.data()===undefined){
+  //  console.log(post_likers)
+
+   }else{
+    liker_list= JSON.parse( JSON.stringify(post_likers.data())); 
+    
+    
+    if (liker_list['liked']=="1"){
+      liked=1
+    }
+
+
+ /*
+   if (user in liker_list){
+    if (liker_list[user]==1){
+    }
+    }
+*/
+   }
+ //  console.log(liker_list);
+    }catch(err){
+      console.log(err);
+    }
       temp.push({"id":documentSnapshot.id,
       "content": documentSnapshot.data().content,
       "timestamp":documentSnapshot.data().timestamp,
       "ownerId":documentSnapshot.data().ownerId,
       "ownerName": documentSnapshot.data().ownerName,
-      "ownerProfilePicture":documentSnapshot.data().ownerProfilePicture
-    
+      "ownerProfilePicture":documentSnapshot.data().ownerProfilePicture,
+      "liked":liked,
+      "liker_list":liker_list,
+      "likecount":like_count
+
+
+
+
+
     });
       setData(temp);
       
@@ -49,52 +104,30 @@ const HomeScreen=({navigation}:any)=>{
   
   var dosomeShit=async()=>{
     accessToken= await AccessToken.getCurrentAccessToken();
-    firestore().collection('posts').onSnapshot(onResult, onError);
+    firestore().collection('posts').orderBy('timestamp','desc').limit(20).onSnapshot(onResult, onError);
    
   
   }
 
 
    var addFieldToDB=async()=>{
-  
-    /*
-   firestore()
-    .collection('posts').doc(auth().currentUser?.uid)
-    .get()
-    .then(querySnapshot => {
-      console.log('Total users: ', querySnapshot.data.length);
-    })
-
-*/
-   
-
     firestore()
     .collection('users').doc(auth().currentUser?.uid).collection("info").doc("0")
     .set({
-      
       name: auth().currentUser?.displayName,
-      phone:"0362785203"
+      verifiedEmail:auth().currentUser?.email,
+      photoURL:auth().currentUser?.photoURL,
+      uid:auth().currentUser?.uid,
+      phoneNumber:auth().currentUser?.phoneNumber
     })
     .then(() => {
-     setCount(count+1);
-      console.log('User added!');
+      console.log('User info added into firestore!');
     });
-/*
-   match /{documents=**}{
-			allow read;
-     allow write;
-    }
-*/
-
-
     +new Date
-    const Jabber = require('jabber');
-    const jabber = new Jabber();
     firestore()
     .collection('posts')
-    .add({
-      
-      content:stt,
+    .add({  
+     content:stt,
       timestamp:Date.now(),
       ownerId: auth().currentUser?.uid,
       ownerName: auth().currentUser?.displayName,
@@ -102,11 +135,9 @@ const HomeScreen=({navigation}:any)=>{
       likeCount: 0
     })
     .then(() => {
-     setCount(count+1);
-      console.log('User added!');
+     setStt("");
+      console.log('Post added!');
     });
-
-    setStt("");
    }
    function timeDifference(current:any, previous:any) {
 
@@ -117,7 +148,9 @@ const HomeScreen=({navigation}:any)=>{
     var msPerYear = msPerDay * 365;
 
     var elapsed = current - previous;
-
+    if (elapsed < 0) {
+      return 'just now';   
+ }
     if (elapsed < msPerMinute) {
          return Math.round(elapsed/1000) + ' seconds ago';   
     }
@@ -144,20 +177,51 @@ const HomeScreen=({navigation}:any)=>{
 }
 
 
-   var addFieldToDB2=async()=>{
-    firestore()
-    .collection('users').doc(auth().currentUser?.uid)
+      var sendLikeToPost= async(id:string,like:number)=>{
+       var uid= await  auth().currentUser?.uid as any; 
+        firestore()
+    .collection("post-likers").doc(id).collection("0").doc(uid)
     .set({
-      id: auth().currentUser?.photoURL ,
-      content: auth().currentUser?.displayName,
+     "liked":like==1?1:0,
     })
     .then(() => {
-     setCount(count+1);
-      console.log('User added!');
+      console.log('sent like!');
     });
+      }
+
+
+ var updateLiked=( id:string,like:number)=>{
+   var tempList=data;
+  
+   var index =0;
+  for(var i=0;i<tempList.length;i++){
+     if(tempList[i].id==id){
+      
+      tempList[i].liked=like==0?1:0;
+      tempList[i].likecount+= like==0?1:-1;
+
+      //console.log(tempList[i].likecount)
+      setData(tempList);
+      +new Date
+      setrd( Date.now);//dont know why flatlist wont update so i decided to do some weird thing to update itself
+     // console.log(data);
+      return;
+     }
+     
    }
+   
+   
+ }
+
   const renderItem = ({item}:any) => {
+    var iconName="heart-outline";
+    var iconColor="#636363"
+   if(item.liked==1){
+     iconName="heart-circle-outline";
+     iconColor="#f41e42"
+   }
     
+   
     +new Date
   //  console.log(  item.ownerProfilePicture+"?access_token=" +accessToken.accessToken);
    // var iconT=  "heart-outline";
@@ -182,29 +246,56 @@ const HomeScreen=({navigation}:any)=>{
     <View  style={{alignSelf:"center",marginLeft:10}}>
      <Text style={{fontWeight:"bold",fontSize:15}}>{item.ownerName}</Text>
 
-     <Text style={{fontStyle:"italic"}}>{timeDifference( Date.now(),item.timestamp)}</Text>
+     <Text style={{fontStyle:"italic",marginBottom:2}}>{timeDifference( Date.now(),item.timestamp)}</Text>
      </View>
      </View>
-     <Text  style={{marginTop:5,fontSize:15}}>{item.content}</Text>
+     <Text  style={{marginTop:5,fontSize:15}}>{item.content + item.likecount}</Text>
      <View  style={{marginTop:5,marginBottom:3,width:"100%",height:2,backgroundColor:"#cdccd2",alignSelf:"center"}}/>
 
 
      
   <View  style={{width:"100%",height:30,flexDirection:"row" ,justifyContent:"space-around"}}>
       
+ <TouchableOpacity
  
-  <Icon  name= {iconName} color="#636363"  size={27}  onPress={()=>{
-  if(iconName==="heart-outline"){
-    setIconName("heart-circle-outline")
-  }else{
-    setIconName("heart-outline")
+ style={{paddingLeft:20,paddingRight:20}}
+  onPress={()=>{
+    // console.log(item.liked);
+     updateLiked(item.id,item.liked);
+      sendLikeToPost(item.id,item.liked);
   }
+    //console.log(liked)
+    }
+ >
+  <Icon style={{margin:0}} name= {iconName} color={iconColor}  size={27} />
+
   
-}
-  //console.log(liked)
-  }/>
+  </TouchableOpacity>
+  <TouchableOpacity
+ 
+ style={{paddingLeft:20,paddingRight:20}}
+  onPress={()=>{
+   
+  }
+    //console.log(liked)
+    }
+ >
   <Icon  name="chatbox-ellipses-outline" color="#636363"  size={27} />
+  </TouchableOpacity>
+  <TouchableOpacity
+ 
+ style={{paddingLeft:20,paddingRight:20}}
+  onPress={()=>{
+    // console.log(item.liked);
+     updateLiked(item.id,item.liked);
+      sendLikeToPost(item.id,item.liked);
+  }
+    //console.log(liked)
+    }
+ >
   <Icon  name="share-social-outline" color="#636363"  size={27} />
+  </TouchableOpacity>
+
 
   </View>
    </View>
@@ -215,57 +306,83 @@ const HomeScreen=({navigation}:any)=>{
 
   return(
     <View>
-<View style={{flexDirection:"row",justifyContent:"space-around"}}>
+     <View style={{flexDirection:"row",justifyContent:"space-around"}}>
 <Button
 title={auth().currentUser?.displayName?.toString()!=null?auth().currentUser?.displayName?.toString():"Bạn chưa đăng nhập"}
-onPress={()=>{auth().signOut().then(() => {navigation.navigate("Login")
-
+onPress={()=>{auth().signOut().then(() => {
+ navigation.navigate("Login")
 LoginManager.logOut()
+
 
   }
 );
 }}
 ></Button>
-      <Button
-      title={"listen database"}
-      onPress={()=>{
-        dosomeShit()
+      
+       
+       
 
-      }}
-      ></Button>
-       <Button
-      title={"add field"}
-      onPress={()=>{
-        addFieldToDB()
 
-      }}
-      ></Button>
       </View>
-      <Input
-      containerStyle={{}}
-      disabledInputStyle={{ backgroundColor: "#ddd" }}
-      inputContainerStyle={{}}
-      value={stt}
 
-      inputStyle={{}}
-      label="What on your mind?"
-      labelStyle={{}}
-      labelProps={{}}
-      leftIcon={<Icon name="happy-outline" size={20} />}
-      leftIconContainerStyle={{}}
-      multiline={true}
-      numberOfLines={3}
-      onChangeText={text=>{
-        setStt(text);
-
-
-      }}
-      placeholder="Post something on newfeed"
-    />
-
-
+    
         <FlatList
+        keyboardShouldPersistTaps="always"
+        ListHeaderComponent={
+          <View style={{flexDirection:"row",}}>
+          <Input
+          containerStyle={{flex:8}}
+          disabledInputStyle={{ backgroundColor: "#ddd" }}
+          inputContainerStyle={{}}
+          value={stt}
+    
+          inputStyle={{}}
+          label="What on your mind?"
+          labelStyle={{}}
+          labelProps={{}}
+          leftIcon={<Icon name="happy-outline" size={20} />}
+          leftIconContainerStyle={{}}
+          multiline={true}
+          numberOfLines={3}
+          onChangeText={text=>{
+            setStt(text);
+    
+    
+          }}
+          
+          placeholder="Post something on newfeed"
+        />
+        <Button
+          buttonStyle={{ width: 80,height:80}}
+          containerStyle={{ margin: 4 ,flex:2,justifyContent:"center" }}
+          disabledStyle={{
+            borderWidth: 2,
+            borderColor: "#00F"
+          }}
+          disabledTitleStyle={{ color: "#00F" }}
+          iconContainerStyle={{ backgroundColor: "#000" }}
+          loadingProps={{ animating: true }}
+          onPress={() => {
+          //  var data2=data;
+          //  data2[0].content="lzo";
+           // setData(data2);
+             addFieldToDB() 
+
+
+          }
+            
+           
+          }
+          title="Share now"
+          titleStyle={{ marginHorizontal: 10 }}
+        />
+    
+    
+    </View>
+
+        }
         data={data}
+        extraData={randomshit}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
        // extraData={selectedId}
